3e6e643a0d34ecbae80f53202a7c394c180aa3c9
LeonidTsoy <74228921+LeonidTsoy@users.noreply.github.com>
2020-12-04 09:11:21 +0500

    Update LibraryApplication.java

--- library/library/src/main/java/com/accountingsystem/library/LibraryApplication.java
+++ library/library/src/main/java/com/accountingsystem/library/LibraryApplication.java
@@ -1,13 +1,39 @@
 package com.accountingsystem.library;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
-@SpringBootApplication
 public class LibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
-	}
+	static final String DB_URL = "jdbc:postgresql://localhost:8080/accountingsystem";
+	static final String USER = "postgres";
+	static final String PASS = "leonid";
 
+	public static void main(String[] argv) {
+
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		Connection connection = null;
+
+		try {
+			connection = DriverManager
+					.getConnection(DB_URL, USER, PASS);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		if (connection != null) {
+			System.out.println("Вы подключены к базе данных");
+		} else {
+			System.out.println("Не удалось подключиться к базе данных");
+		}
+	}
 }
